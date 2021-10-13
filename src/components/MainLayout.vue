405305627a9b9f66c8bdc1a838e8ba3aa9534d31
Nikita Mikryukov <senkone0123@gmail.com>
2021-10-13 14:45:32 +0500

    Starting server Vue3 + babel + ESLint + Dart-Sass + BalmUi

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ui-button>12345</ui-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+
+})
+
+export default class MainLayout extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
