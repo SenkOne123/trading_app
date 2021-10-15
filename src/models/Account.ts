1f0e6a90ebcd3673896c63b3765f72ae62f6df8c
Nikita Mikryukov <senkone0123@gmail.com>
2021-10-15 16:45:27 +0500

    deploying to cloudflare

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -3,15 +3,16 @@ import Currencies from "@/models/Currencies";
 
 export default class Account {
   reality: BillStates = BillStates.DEMO;
-  login: number = 0;
-  currency: Currencies;
-  leverage: number = 0;
-  balance: number = 0;
-  equity: number = 0;
-  margin: number = 0;
-  freeMargin: number = 0;
-  marginLevel: number = 0;
-  code: number = 0;
-  message: string = "";
-  webRequestId: string = "";
+  login = 0;
+  balance = 0;
 }
+
+// currency: Currencies;
+// leverage: number = 0;
+// equity: number = 0;
+// margin: number = 0;
+// freeMargin: number = 0;
+// marginLevel: number = 0;
+// code: number = 0;
+// message: string = "";
+// webRequestId: string = "";
