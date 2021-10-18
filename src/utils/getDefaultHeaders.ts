6af207dff2a3e2c0512d2b322067dbdaccb6ee6b
Nikita Mikryukov <senkone0123@gmail.com>
2021-10-18 15:45:44 +0500

    CORS!

--- src/utils/getDefaultHeaders.ts
+++ src/utils/getDefaultHeaders.ts
@@ -1,8 +1,8 @@
 export function getHeaders(token?: string): Headers {
   return new Headers({
     "Content-Type": "application/json; charset=UTF-8",
-    "Access-Control-Allow-Origin": "https://simplefx.com/docs/api/swagger/",
     Authorization: "Bearer " + token,
     Accept: "application/json",
+    "Access-Control-Allow-Origin": "https://rest.simplefx.com/",
   });
 }
