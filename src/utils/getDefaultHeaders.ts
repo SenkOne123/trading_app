98a089190b59ed448ab8bfd7746c4e312bb98ed1
Nikita Mikryukov <senkone0123@gmail.com>
2021-10-18 15:36:57 +0500

    deploying to Github Pages

--- src/utils/getDefaultHeaders.ts
+++ src/utils/getDefaultHeaders.ts
@@ -1,6 +1,7 @@
 export function getHeaders(token?: string): Headers {
   return new Headers({
     "Content-Type": "application/json; charset=UTF-8",
+    "Access-Control-Allow-Origin": "https://senkone123.github.io/",
     Authorization: "Bearer " + token,
     Accept: "application/json",
   });
