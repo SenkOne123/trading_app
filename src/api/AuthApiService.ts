0f87de01bf80edee74a1fb75872756e891a6628c
Nikita Mikryukov <senkone0123@gmail.com>
2021-10-18 17:12:53 +0500

    +proxy anywhere cors

--- src/api/AuthApiService.ts
+++ src/api/AuthApiService.ts
@@ -2,15 +2,17 @@ import { getHeaders } from "@/utils/getDefaultHeaders";
 
 export default class AuthApiService {
   getAuthentificationToken(): Promise<any> {
-    return fetch("https://rest.simplefx.com/api/v3/auth/key", {
-      method: "POST",
-      mode: "no-cors",
-      headers: getHeaders(),
-      body: JSON.stringify({
-        clientId: "7258c3d936e346eca2f8f23ee27c1bfb",
-        clientSecret: "05b4de65-03c8-43a9-8709-97db662db33b",
-      }),
-    })
+    return fetch(
+      "https://intense-thicket-79155.herokuapp.com/https://rest.simplefx.com/api/v3/auth/key",
+      {
+        method: "POST",
+        headers: getHeaders(),
+        body: JSON.stringify({
+          clientId: "7258c3d936e346eca2f8f23ee27c1bfb",
+          clientSecret: "05b4de65-03c8-43a9-8709-97db662db33b",
+        }),
+      }
+    )
       .then((response) => response.json())
       .catch((err) => err.json);
   }
